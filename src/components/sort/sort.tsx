63bff478a99bfba227bb3ab1660ec92a44697aa9
Ve1heora <fareast.vladivostok.chuchalin@gmail.com>
2024-09-17 20:34:35 +1000

    fix:  правки

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -28,7 +28,7 @@ export const Sort = (): JSX.Element => {
     >
       <span className="places__sorting-caption">Sort by</span>
       <span className="places__sorting-type" tabIndex={0}>
-        {activeSort}
+        {' '}{activeSort}{' '}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
